44dd9e58329e4458542bb684427c5427c0380928
noah <jeonghogim318@gmail.com>
2024-06-13 20:35:57 +0900

    setting

---
+++ src/main/java/com/example/noexcuse/NoexcuseApplication.java
@@ -0,0 +1,13 @@
+package com.example.noexcuse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoexcuseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoexcuseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/noexcuse/NoexcuseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.noexcuse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoexcuseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
